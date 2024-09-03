5b4d6b50872bb8babe4d05d35c09d905f023c40b
MPT <159217501+mr-mpt@users.noreply.github.com>
2024-09-02 21:34:25 -0600

    Create femboy.py

---
+++ src/femboy.py
@@ -0,0 +1,13 @@
+"""
+This is the source code for Femboy.py
+"""
+
+class Femboy:
+    def cuddle():
+        return "purrrrrr!"
+    def kiss():
+        return "mwah!!!"
+    def pet():
+        return "purrrrrr!"
+    def hit():
+        return "owwie :("
